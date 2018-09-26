56c035eab695d98e22dd1b0df87b1fb16332f29c
tribeiro <tribeiro@sedronar.gov.ar>
2018-09-26 17:03:48 -0300

    [UPDATE] template section c6

--- angular-project/src/app/tomi/components/mensual/mensual-seccion-c6/mensual-seccion-c6.component.ts
+++ angular-project/src/app/tomi/components/mensual/mensual-seccion-c6/mensual-seccion-c6.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {HeaderEvent} from "../../../../model/header-event";
+import {LoadingComponent} from "../../../../share-components/loading/loading.component";
 
 @Component({
   selector: 'app-mensual-seccion-c6',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mensual-seccion-c6.component.css']
 })
 export class MensualSeccionC6Component implements OnInit {
+  private readonlyControl: boolean = true;
+  private formMessages: string[] = [];
+  private errorSection: number = -1;
+  private hojaId: number;
+  @ViewChild(LoadingComponent) loadingComponent:LoadingComponent;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private onChangeHeader(headerEvent: HeaderEvent) {
+    if (headerEvent.evento == HeaderEvent.HOJA_ID) {
+      this.readonlyControl = headerEvent.value.hojaId == null;
+      this.hojaId = headerEvent.value.hojaId;
+      if (headerEvent.value.hojaId) {
+        // this.loadingComponent.showLoading();
+        // this.mensualSeccionCService.findDataSeccionC5ByHojaId(headerEvent.value.hojaId).subscribe(data => this.parseSeccionC5Data(data));
+      } else {
+        // this.initEmptyData();
+      }
+    }
   }
 
 }
