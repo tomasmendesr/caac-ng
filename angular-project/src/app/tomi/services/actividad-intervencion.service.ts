ed0e9acec1ed7ce0ef97d3f76da1d9295125d163
tribeiro <tribeiro@sedronar.gov.ar>
2018-10-02 10:13:27 -0300

    [UPDATE] complete list

---
+++ angular-project/src/app/tomi/services/actividad-intervencion.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Mes} from "../../model/mes";
+import {Observable} from "rxjs/Observable";
+import {UrlConstants} from "./UrlConstants";
+
+@Injectable()
+export class ActividadIntervencionService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Mes[]>{
+    return this.http.post<Mes[]>(UrlConstants.FIND_ALL_MESES, {});
+  }
+
+
+}
